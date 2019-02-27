a54d21798697ca892a5ef30eec8b2be6670bfb7b
Mat King <mat@matking.info>
2019-02-27 14:34:34 -0500

    phase2

--- app/config/orm.js
+++ app/config/orm.js
@@ -19,57 +19,54 @@ var connection = require("../config/connection.js");
 
 //    * Export the ORM object in `module.exports`.
 
-
-// // Object for all our SQL statement functions.
-// var orm = {
-//   all: function(tableInput, cb) {
-//     var queryString = "SELECT * FROM " + tableInput + ";";
-//     connection.query(queryString, function(err, result) {
-//       if (err) {
-//         throw err;
-//       }
-//       cb(result);
-//     });
-//   },
-//   create: function(table, cols, vals, cb) {
-//     var queryString = "INSERT INTO " + table;
-
-//     queryString += " (";
-//     queryString += cols.toString();
-//     queryString += ") ";
-//     queryString += "VALUES (";
-//     queryString += printQuestionMarks(vals.length);
-//     queryString += ") ";
-
-//     console.log(queryString);
-
-//     connection.query(queryString, vals, function(err, result) {
-//       if (err) {
-//         throw err;
-//       }
-
-//       cb(result);
-//     });
-//   },
-//   // An example of objColVals would be {name: panther, sleepy: true}
-//   update: function(table, objColVals, condition, cb) {
-//     var queryString = "UPDATE " + table;
-
-//     queryString += " SET ";
-//     queryString += objToSql(objColVals);
-//     queryString += " WHERE ";
-//     queryString += condition;
-
-//     console.log(queryString);
-//     connection.query(queryString, function(err, result) {
-//       if (err) {
-//         throw err;
-//       }
-
-//       cb(result);
-//     });
-//   }
-// };
+var orm = {
+    selectAll: function(table, cb) {
+        var queryString = "SELECT * FROM " + table;
+        connection.query(queryString, function(err, result) {
+        if (err) {
+            throw err;
+        }
+        cb(result);
+        });
+    },
+    insertOne: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+    
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+    
+        console.log(queryString);
+    
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+            throw err;
+            }
+    
+            cb(result);
+        });
+    },
+    updateOne: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+    
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+            if (err) {
+            throw err;
+            }
+    
+            cb(result);
+        });
+    }
+};
 
 // Export the orm object for the model (cat.js).
 module.exports = orm;
--- app/controllers/burgers_controller.js
+++ app/controllers/burgers_controller.js
@@ -1,9 +1,54 @@
-// * Inside your `burger` directory, create a folder named `models`.
+// 1. Inside your `burger` directory, create a folder named `controllers`.
 
-//   * In `models`, make a `burger.js` file.
+// 2. In `controllers`, create the `burgers_controller.js` file.
 
-//     * Inside `burger.js`, import `orm.js` into `burger.js`
+// 3. Inside the `burgers_controller.js` file, import the following:
 
-//     * Also inside `burger.js`, create the code that will call the ORM functions using burger specific input for the ORM.
+//    * Express
+//    * `burger.js`
+var express = require("express");
 
-//     * Export at the end of the `burger.js` file.
+var router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+var burgerCon = require("../models/burger");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+    burgerCon.all(function(data) {
+        var hbsObject = {
+        cats: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burger", function(req, res) {
+    burgerCon.create(["name", "sleepy"], [req.body.name, req.body.sleepy], function(result) {
+    // Send back the ID of the new quote
+        res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burger/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+  console.log("condition", condition);
+  burgerCon.update(
+    {
+      sleepy: req.body.sleepy
+    },
+    condition,
+    function(result) {
+      if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
+      res.status(200).end();
+
+    }
+  );
+});
+
+// Export routes for server.js to use.
+module.exports = router;
